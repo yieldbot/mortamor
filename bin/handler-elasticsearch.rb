1bf7696f62a40e2adc993c293465db4f258596b7
Matt Jones <mattjones@yieldbot.com>
2015-09-12 00:56:18 -0400

    first pass at handler, no mappings

--- bin/handler-elasticsearch.rb
+++ bin/handler-elasticsearch.rb
@@ -2,3 +2,16 @@
 #
 # this will take json and put it into elasticsearch for longterm storage
 #
+#
+
+class DetailedMailer < Sensu::Handler
+  def handle
+    data = @event['check']
+
+    File.open('/tmp/matty', 'w') do |f|
+      f.write(data)
+    end
+
+    `curl -XPUT "http://localhost:9200/sensu/?pretty" -d data`
+  end
+end
