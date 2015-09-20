f423237a57d8537b05d26b8cf6d5c80a12cdd8eb
Matt Jones <mattjones@yieldbot.com>
2015-09-20 00:52:31 -0400

    working status handler

--- bin/handler-elasticsearch-status.rb
+++ bin/handler-elasticsearch-status.rb
@@ -10,22 +10,29 @@ require 'digest/md5'
 require 'date'
 
 class ElasticsearchMetrics < Sensu::Handler
-  def es_host
-    settings['elasticsearch']['host'] || 'localhost'
+  def acquire_setting(name)
+    product = ARGV[0]
+    settings[product][name]
   end
 
-  def es_port
-    settings['elasticsearch']['port'] || 9200
-  end
+  # def es_host
+  #   product = ARGV[0]
+  #   settings[product]['elasticsearch']['host'] || 'localhost'
+  # end
 
-  def calc_date
-    ts = @event['client']['timestamp']
-    ts.utch
-  end
+  # def es_port
+  #   settings['elasticsearch']['port'] || 9200
+  # end
 
-  def es_index
-    settings['elasticsearch']['index'] || 'monitoring-status'
-  end
+  # def calc_date
+  #   ts = @event['client']['timestamp']
+  #   ts.utch
+  # end
+
+  # def es_index
+  #   settings['elasticsearch']['index'] || 'monitoring-status'
+  # end
+
   #   def es_id
   #     rdm = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(3).join
   #     Digest::MD5.new.update("#{rdm}")
@@ -40,15 +47,15 @@ class ElasticsearchMetrics < Sensu::Handler
     @event['client']['name'] + '/' + @event['check']['name']
   end
 
-  def es_type
-    if @event['check']['elasticsearch']
-      data = @event['check']['elasticsearch']
-      "#{data['source']}-#{data['location']}-#{data['type']}"
-    else
-      puts 'Elasticsearch index path not found'
-      exit(127)
-    end
-  end
+  # def es_type
+  #   if @event['check']['elasticsearch']
+  #     data = @event['check']['elasticsearch']
+  #     "#{data['source']}-#{data['location']}-#{data['type']}"
+  #   else
+  #     puts 'Elasticsearch index path not found'
+  #     exit(127)
+  #   end
+  # end
 
   # def service_output
   #   out = JSON.parse(@event['[check']['output'])
@@ -57,36 +64,39 @@ class ElasticsearchMetrics < Sensu::Handler
   # end
 
   def handle
-    data = {
-      timestamp: calc_date,
-      client: @event['client']['name'],
-      check_name: @event['check']['name'],
-      status: @event['check']['status'],
-      address: @event['client']['address'],
-      command: @event['check']['command'],
-      occurrences: @event['occurrences'],
-      output: @event['output']
-    }
+    # data = {
+    #   timestamp: calc_date,
+    #   client: @event['client']['name'],
+    #   check_name: @event['check']['name'],
+    #   status: @event['check']['status'],
+    #   address: @event['client']['address'],
+    #   command: @event['check']['command'],
+    #   occurrences: @event['occurrences'],
+    #   output: @event['output']
+    # }
 
-    begin
-      timeout(5) do
-        uri = URI("http://#{es_host}:#{es_port}/#{index}/general")
-        http = Net::HTTP.new(uri.host, uri.port)
-        request = Net::HTTP::Post.new(uri.path, 'content-type' => 'application/json; charset=utf-8')
-        request.body = JSON.dump(data)
-        response = http.request(request)
-        if response.code == '200|201'
-          puts "request metrics #=> #{data}"
-          puts "request body #=> #{response.body}"
-          puts 'elasticsearch post ok.'
-        else
-          puts "request metrics #=> #{data}"
-          puts "request body #=> #{response.body}"
-          puts "elasticsearch post failure. status error code #=> #{response.code}"
-        end
+    timeout(5) do
+      host   = acquire_setting(host)
+      port   = acquire_setting(port)
+      index  = acquire_setting(index)
+
+      uri           = URI("http://#{host}:#{port}/#{index}")
+      http          = Net::HTTP.new(uri.host, uri.port)
+      request       = Net::HTTP::Post.new(uri.path, 'content-type' => 'application/json; charset=utf-8')
+      request.body  = JSON.dump(@event)
+      response      = http.request(request)
+
+      if response.code == '200|201'
+        puts "request data #=> #{@event}"
+        puts "request body #=> #{response.body}"
+        puts 'elasticsearch post ok.'
+      else
+        puts "request data #=> #{@event}"
+        puts "request body #=> #{response.body}"
+        puts "elasticsearch post failure. status error code #=> #{response.code}"
       end
-    rescue Timeout::Error
-      puts 'elasticsearch timeout error.'
     end
+  rescue Timeout::Error
+    puts 'elasticsearch timeout error.'
   end
 end
