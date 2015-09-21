cb3ae2b3a4bb2f94113f7f3767ac08a62d743a3b
Matt Jones <mattjones@yieldbot.com>
2015-09-21 10:39:21 -0400

    add new info

--- bin/handler-elasticsearch-status.rb
+++ bin/handler-elasticsearch-status.rb
@@ -15,66 +15,73 @@ class ElasticsearchMetrics < Sensu::Handler
     settings[product][name]
   end
 
-  # def es_host
-  #   product = ARGV[0]
-  #   settings[product]['elasticsearch']['host'] || 'localhost'
-  # end
-
-  # def es_port
-  #   settings['elasticsearch']['port'] || 9200
-  # end
-
-  # def calc_date
-  #   ts = @event['client']['timestamp']
-  #   ts.utch
-  # end
-
-  # def es_index
-  #   settings['elasticsearch']['index'] || 'monitoring-status'
-  # end
-
-  #   def es_id
-  #     rdm = ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample(3).join
-  #     Digest::MD5.new.update("#{rdm}")
-  #   end
-
-  #   def time_stamp
-  #     d = DateTime.now
-  #     d.to_s
-  #   end
-
   def event_name
     @event['client']['name'] + '/' + @event['check']['name']
   end
 
-  # def es_type
-  #   if @event['check']['elasticsearch']
-  #     data = @event['check']['elasticsearch']
-  #     "#{data['source']}-#{data['location']}-#{data['type']}"
-  #   else
-  #     puts 'Elasticsearch index path not found'
-  #     exit(127)
-  #   end
-  # end
+  def acquire_monitored_instance
+    @event['client']['name']
+  end
 
-  # def service_output
-  #   out = JSON.parse(@event['[check']['output'])
-  #   service = out['service']
-  #   status = out['status']
-  # end
+  def acquire_infra_details
+    JSON.parse(File.read('/etc/sensu/conf.d/monitoring_infra.json'))
+  end
+
+  def define_status(event)
+    case event
+    when 0
+      return 'OK'
+    when 1
+      return 'WARNING'
+    when 2
+      return 'CRITICAL'
+    when 3
+      return 'UNKNOWN'
+    when 127
+      return 'CONFIG ERROR'
+    when 126
+      return 'PERMISSION DENIED'
+    else
+      return 'ERROR'
+    end
+  end
+
+  def define_sensu_env
+    case acquire_infra_details['sensu']['environment']
+    when 'prd'
+      return 'Prod: '
+    when 'dev'
+      return 'Dev: '
+    when 'stg'
+      return 'Stg: '
+    when 'vagrant'
+      return 'Vagrant: '
+    else
+      return 'Test: '
+    end
+  end
+
+  def define_check_state_duration
+    ''
+  end
+
+  def check_data
+    ''
+  end
 
   def handle
-    # data = {
-    #   timestamp: calc_date,
-    #   client: @event['client']['name'],
-    #   check_name: @event['check']['name'],
-    #   status: @event['check']['status'],
-    #   address: @event['client']['address'],
-    #   command: @event['check']['command'],
-    #   occurrences: @event['occurrences'],
-    #   output: @event['output']
-    # }
-
+    data = {
+      'monitored_instance'    => acquire_monitored_instance, # this will be the snmp host if using traps
+      'sensu_client'          => @event['client']['name'],
+      'incident_timestamp'    => Time.at(@event['check']['issued']),
+      'instance_address'      => @event['client']['address'],
+      'check_name'            => @event['check']['name'],
+      'check_state'           => define_status(@event['check']['status']),
+      'check_data'            => check_data, # any additional user supplied data
+      'sensu_env'             => define_sensu_env,
+      'check_state_duration'  => define_check_state_duration
+    }
+    
     timeout(5) do
       host   = acquire_setting('host')
       port   = acquire_setting('port')
@@ -84,15 +91,15 @@ class ElasticsearchMetrics < Sensu::Handler
       uri           = URI("http://#{host}:#{port}/#{index}/dashboard/#{id}")
       http          = Net::HTTP.new(uri.host, uri.port)
       request       = Net::HTTP::Post.new(uri.path, 'content-type' => 'application/json; charset=utf-8')
-      request.body  = JSON.dump(@event)
+      request.body  = JSON.dump(data)
       response      = http.request(request)
 
       if /200|201/ =~ response.code
-        puts "request data #=> #{@event}"
+        puts "request data #=> #{data}"
         puts "request body #=> #{response.body}"
         puts 'elasticsearch post ok.'
       else
-        puts "request data #=> #{@event}"
+        puts "request data #=> #{data}"
         puts "request body #=> #{response.body}"
         puts "elasticsearch post failure. status error code #=> #{response.code}"
       end
